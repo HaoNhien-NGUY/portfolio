1bbdb4840ec0fcbe8fd865ab96d87895a9a404cf
hao-nhien.nguyen <hao-nhien.nguyen@epitech.eu>
2020-07-13 14:17:22 +0200

    added social in nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React, { useRef, useEffect, useState } from 'react';
+import { useSpring, animated, config } from 'react-spring';
+
 import './navigation.css';
+import linkedInImg from '../../assets/linkedInImg2.png';
+import cvImg from '../../assets/curriculum-vitae.png';
+import gitHubImg from '../../assets/GitHub-Mark-64px.png';
+import cvPdf from '../../assets/CV_HaoNhien-NGUYEN.pdf';
+
 
 function Navigation(props) {
     const { pageAnchor, inView } = props;
     const navWrapper = useRef();
     const navItems = useRef();
     const [activeLi, setActiveLi] = useState();
+
+    const [socialAppear, setSocialAppear] = useSpring(() => ({ opacity: 0, right: "-200px", config: {...config.default} }));
 
     function toggle() {
         navWrapper.current.classList.toggle('navbar-small');
@@ -22,6 +31,10 @@ function Navigation(props) {
 
     useEffect(() => {
         setActiveLi(pageAnchor.destination);
+        if (pageAnchor.origin === 'accueil' && pageAnchor.destination !== 'accueil')
+            setSocialAppear({ opacity: 1, right: "0px" });
+        if (pageAnchor.origin !== 'accueil' && pageAnchor.destination === 'accueil')
+            setSocialAppear({ opacity: 0, right: "-200px" });
     }, [pageAnchor])
 
     return (
@@ -42,6 +55,13 @@ function Navigation(props) {
                             <a href="#contact" className={`noselect contact ${activeLi === "contact" ? "selected" : null}`} >Contact</a>
                         </li>
                     </ul>
+                </div>
+                <div className="nav-socials noselect">
+                    <animated.div className="imgs" style={socialAppear}>
+                        <a href="https://github.com/HaoNhien-NGUY"><img src={gitHubImg}></img></a>
+                        <a href="https://www.linkedin.com/in/haonhien-nguyen/" target="_blanc" rel="noopener noreferrer"><img src={linkedInImg}></img></a>
+                        <a href={cvPdf} target="_blanc" rel="noopener noreferrer"><img src={cvImg}></img></a>
+                    </animated.div>
                 </div>
             </div>
         </>
